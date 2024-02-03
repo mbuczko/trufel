12bc60f6a196084207d8ba06d20e29f48dbb608d
Michal Buczko <michal@buczko.pl>
2024-02-04 00:22:59 +0100

    initial TabsBar and Tab components

---
+++ webapp/lib/Tab.svelte
@@ -0,0 +1,22 @@
+<script>
+import { getContext } from 'svelte';
+
+/** @type {String} */
+export let id;
+
+/** @type {String} */
+export let label;
+
+/** Notifies a parent about being selected */
+const select = (getContext('tabs') || {}).select;
+
+getContext('tabs').registerTab();
+
+</script>
+
+<button id={id}         
+        type="button" 
+        class="relative z-20 inline-flex items-center justify-center w-full h-8 px-3 text-sm font-medium transition-all rounded-md cursor-pointer whitespace-nowrap"
+        on:click={() => { if (select) select(id) }}>
+    {label}
+</button>
