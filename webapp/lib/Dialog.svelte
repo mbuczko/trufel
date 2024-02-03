a906281f319cfe090d58842303e048c4c02c6b76
Michal Buczko <michal@buczko.pl>
2024-02-04 00:24:24 +0100

    improve Dialog view

--- webapp/lib/Dialog.svelte
+++ webapp/lib/Dialog.svelte
@@ -1,106 +1,63 @@
-<div class="dialog p-5 w-full min-w-80 max-w-screen-md bg-white border rounded-lg shadow-md">
-    <div class="header border-b">
-        <h2> Header </h2>
+<script>
+import Toggle from '$lib/Toggle.svelte';
+import IconsGallery from './IconsGallery.svelte';
+
+/** @type {boolean} */
+let iconSearch = false;
+
+</script>
+
+<div class="dialog flex min-w-80 max-w-screen-md shadow-md border-1">
+    <div class="sidebar grid grid-cols-[64px_1fr] justify-items-start bg-slate-500 rounded-tl-lg rounded-bl-lg p-2">
+        <button
+            class="self-start icon w-14 h-14 border-2 border-rose-500"
+            on:click="{() => iconSearch = !iconSearch}" />
+        <div class="self-start pt-2 min-w-40">
+            <h3 class="text-gray-800 font-medium uppercase"> Proxmox </h3>
+            <div class="text-xs text-gray-700"> Clustered Virtual Machines </div>
+        </div>
+        <div class="col-span-2 text-left">
+            <Toggle id="shared" label="Shared with others" enabled={false} />
+        </div>
+        <div class="col-span-2 text-left">
+            <Toggle id="searchable" label="Searchable" enabled={false} />
+        </div>
+        <div class="col-span-2 text-left">
+            <Toggle id="disabled" label="Disabled" enabled={false} />
+        </div>
     </div>
-    <div class="top-content px-3">
-        <div class="icon"/>
-        <div class="details">
-            <h3> Proxmox </h3>
-            <div>
-                Self-hosted Proxmox cluster
+    <div class="details bg-white border-1 rounded-tr-lg rounded-br-lg p-4">
+        {#if iconSearch}
+            <div class="">
+                <IconsGallery />
             </div>
-        </div>
+        {:else}
+            <form>
+                <label for="url"> URL </label>
+                <input name="url" id="url" type="text"/>
+
+
+                <label for="title"> Title </label>
+                <input name="title" id="title" type="text"/>
+
+                <label for="description"> Description </label>
+                <input name="description" id="description" type="text"/>
+
+                <div class="form-buttons">
+                    <button> Cancel </button>
+                    <input name="submit" type="submit" value="Save" class="submit-button" />
+                </div>
+            </form>
+        {/if}
     </div>
-    <form class="main-content">
-        <label for="url"> URL: </label>
-        <input name="url" id="url" type="text"/>
-
-        <label for="title"> Title: </label>
-        <input name="title" id="title" type="text"/>
-
-        <label for="description"> Description: </label>
-        <input name="description" id="description" type="text"/>
-
-        <label for="shared"> Shared: </label>
-        <input name="shared" id="shared" type="checkbox"/>
-
-        <div class="form-buttons">
-            <button> Cancel </button>
-            <input name="submit" type="submit" value="Save" class="submit-button" />
-        </div>
-    </form>
 </div>
 
 <style>
- .dialog {
-     margin: 0 20%;
-     --dialog-border: 1px solid rgb(0, 0, 200, 0.5);
+ .sidebar :last-child {
+     margin-bottom: 40px;
  }
- .header {
-     width: 100%;
-     padding: 8px 0;
-     text-transform: uppercase;
- }
- .top-content {
-     display: flex;
-     flex-wrap: wrap;
-     align-items: flex-start;
-     padding: 10px;
-     margin-left: 95px;
-     gap: 10px;
- }
-
- .main-content {
-     display: grid;
-     gap: 4px;
-     grid-template-columns: 100px 1fr;
- }
-
- .icon {
-     width: 64px;
-     height: 64px;
-     border: 1px solid red;
- }
-
- label {
-     text-align: end;
-     padding: 4px;
- }
- input[type=text] {
-     border: var(--dialog-border);
-     border-radius: 3px;
-     padding: 4px;
-     width: 100%;
- }
- input[type=checkbox] {
-     width: 30px;
- }
- input[type=submit] {
-     background: #55f;
-     border: var(--dialog-border);
-     border-radius: 3px;
-     color: white;
- }
- input:hover {
-     border-color: rgb(0, 0, 200, 0.9);
- }
- 
- .form-buttons {
-     grid-column: span 2;
-     margin-top: 10px;
-     display: flex;
-     flex-wrap: wrap;
-     justify-content: flex-end;
-     gap: 10px;
- }
- 
- .form-buttons button {
-     background: white;
-     border: var(--dialog-border);
-     border-radius: 3px;
- }
- 
- .form-buttons > * {
-     min-width: 100px;
+ .details {
+     min-width: 410px;
+     min-height: 270px;
  }
 </style>
